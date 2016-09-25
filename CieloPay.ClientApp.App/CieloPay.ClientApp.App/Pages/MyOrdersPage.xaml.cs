d7d08a5c063c007e135a7adffe123dc77be3d399
Alexsander Peguim <AlxPeguim@Alexsanders-MacBook-Air.local>
2016-09-25 06:07:00 -0300

    User

--- CieloPay.ClientApp.App/CieloPay.ClientApp.App/Pages/MyOrdersPage.xaml.cs
+++ CieloPay.ClientApp.App/CieloPay.ClientApp.App/Pages/MyOrdersPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using ImageCircle.Forms.Plugin.Abstractions;
+using Xamarin.Forms;
 
 namespace CieloPay.ClientApp.App.Pages
 {
@@ -8,6 +9,26 @@ namespace CieloPay.ClientApp.App.Pages
         public MyOrdersPage()
         {
             InitializeComponent();
+
+			var stackLayout = new StackLayout();
+			var img = new StackLayout();
+			img.Padding = 40;
+			var vetProfileImage = new CircleImage
+			{
+				BorderColor = Color.FromRgb(4, 175, 240),
+				BorderThickness = 2,
+				HeightRequest = 100,
+				WidthRequest = 100,
+				Aspect = Aspect.AspectFill,
+				VerticalOptions = LayoutOptions.Center,
+			};
+			vetProfileImage.SetBinding(Image.SourceProperty, "Image");
+
+			img.Children.Add(vetProfileImage);
+			stackLayout.Children.Add(img);
+
+			this.Content = stackLayout;
+
 
 		}
 
