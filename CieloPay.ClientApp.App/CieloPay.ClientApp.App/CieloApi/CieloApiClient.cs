3453ecf45581b895dac29cc5efec001e74f6b898
Paulo Henrique da Silva Fernandes <pfernandes@braspag.com.br>
2016-09-25 01:53:05 -0300

    a

---
+++ CieloPay.ClientApp.App/CieloPay.ClientApp.App/CieloApi/CieloApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CieloPay.ClientApp.App.CieloApi.Model;
+using Newtonsoft.Json;
+
+namespace CieloPay.ClientApp.App.CieloApi
+{
+    public class CieloApiClient
+    {
+        public async Task<Sale> PostSaleAsync(Sale sale)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.cieloecommerce.cielo.com.br");
+                client.DefaultRequestHeaders.Add("MerchantId", "EE2A058F-FF53-4B64-B0B2-A8ECE7C827D6");
+                client.DefaultRequestHeaders.Add("MerchantKey", "keR1RauP3RA1TNheCiauTD0U5vsoOyJX0poDqyib");
+
+                var json = JsonConvert.SerializeObject(sale);
+
+                var response = await client.PostAsync("/1/sales", new StringContent(json, Encoding.UTF8, "text/json"));
+                var strResponse = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<Sale>(strResponse);
+                }
+
+                return sale;
+            }
+        }
+    }
+}
---
+++ CieloPay.ClientApp.App/CieloPay.ClientApp.App/CieloApi/Model/Sale.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms.Pages;
+
+namespace CieloPay.ClientApp.App.CieloApi.Model
+{
+    public class Sale
+    {
+        public string MerchantOrderId { get; set; }
+        public Customer Customer { get; set; }
+        public Payment Payment { get; set; }
+    }
+
+    public class Payment
+    {
+        public string Type => "CreditCard";
+        public string Provider => "Simulado";
+        public int Installments { get; set; }
+        public long Amount { get; set; }
+        public Card Card { get; set; }
+        public byte Status { get; set; }
+    }
+
+    public class Card
+    {
+        public string Holder { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+        public string Brand { get; set; }
+        public bool SaveCard { get; set; }
+        public Guid CardToken { get; set; }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+}
